2718bf24adf46278793e737a98ebdcce4ad3677c
rita1125 <chenyenzu1125@gmail.com>
2024-10-23 22:06:45 +0800

    換成用jawDB在heroku

--- connMysql.php
+++ connMysql.php
@@ -1,37 +1,30 @@
-<?php
-    //Heroku環境
-  if(getenv("CLEARDB_DATABASE_URL")){
-      //ClearDB
-      $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-      $host = $cleardb_url["host"];
-      $username = $cleardb_url["user"];
-      $password = $cleardb_url["pass"];
-      $db = substr($cleardb_url["path"], 1);
-      
-      //資料庫連線
-      $link = new mysqli($host, $username, $password, $db);  //new mysqli()->物件導向方式
-      if ($link->connect_error) {
+<?php 
+  // Heroku環境
+if (getenv("JAWSDB_URL")) {
+    //JawsDB
+    $jawsdb_url = parse_url(getenv("JAWSDB_URL"));
+    $host = $jawsdb_url["host"];
+    $username = $jawsdb_url["user"];
+    $password = $jawsdb_url["pass"];
+    $db = ltrim($jawsdb_url["path"], '/'); // 去掉路徑前的斜杠
+
+    //資料庫連線
+    $link = new mysqli($host, $username, $password, $db); // new mysqli() 物件導向方式
+    if ($link->connect_error) {
         die("資料連結失敗: " . $link->connect_error);
-      }
-  } else {
-      //本機(XAMPP)
-      $db_host = "localhost";
-      $db_name = "album";
-      $db_username = "root";
-      $db_password = "";
+    }
+} else {
+    //本機XAMPP
+    $db_host = "localhost";
+    $db_name = "album";
+    $db_username = "root";
+    $db_password = "";
 
-      //資料庫連線
-      $link = new mysqli($db_host, $db_username, $db_password, $db_name);
-      $link->set_charset("utf8");
-      if($link->connect_error){
-        die("資料連結失敗".mysqli_connect_error());
-      }
-      // $link = mysqli_connect($db_host, $db_username, $db_password);  //mysqli_connect()->程序式方式
-      // if(!$link){
-      //   die("資料連結失敗".mysqli_connect_error());
-      // }
-      // mysqli_select_db($link,$db_name);
-      // mysqli_query($link,"SET NAMES 'utf8'");
-  }
+    //資料庫連線
+    $link = new mysqli($db_host, $db_username, $db_password, $db_name);
+    $link->set_charset("utf8");
+    if ($link->connect_error) {
+        die("資料連結失敗: " . $link->connect_error);
+    }
+}
 ?>
-
